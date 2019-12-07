6321b0c302978466562b9cec2c54db2f7348204a
Cédric <soheib.boudali29@gmail.com>
2019-12-07 02:42:31 +0100

    create vectorial fuction : produit_interne

--- vectorial.py
+++ vectorial.py
@@ -19,3 +19,22 @@ def change(file):
 	return liste
 file=change("weights_reversed.txt")
 
+def produit_interne(file , requete):
+	requete=requete.split()
+	i=1
+	pertinant_doc=[]
+	while i < 3205:
+		document=[line for line in file if int(line["dic"])==(i)]
+		poid=0
+		for doc in document:
+			for word in requete:
+				if word.lower()==doc["word"]:
+					poid+=float(doc["weight"])
+		if poid != 0:
+			pertinant_doc.append("document: "+str(i)+" poid : "+str(poid))
+		i+=1
+	if len(pertinant_doc) == 0:
+		return False
+	else:
+		return pertinant_doc
+
